802424543062f93a44609379cf69bd80e933a6da
João Pedro <joaof082@gmail.com>
2025-05-10 19:53:17 -0300

    feat: starting building slice of application to task
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/redux/slices/tasksSlice.ts
+++ bennuTaskApp/src/redux/slices/tasksSlice.ts
@@ -1,1 +1,28 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {IListTask} from '../store/types';
+import * as TaskService from '../../services/TaskService';
+
+const initialState: IListTask = {
+  items: [],
+  loading: false,
+  error: 'null',
+};
+
+export const fetchTasks = createAsyncThunk(
+  'task/fetchTasks',
+  TaskService.fetchTasks,
+);
+
+const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchTasks.pending, state => {
+      state.loading = true;
+      state.error = 'null';
+    });
+  },
+});
+
+export default taskSlice.reducer;
