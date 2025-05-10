65e68687a5c9258c122c86b3be1c6f19a9ad8b7b
João Pedro <joaof082@gmail.com>
2025-05-10 18:27:17 -0300

    feat: addin new route of create new task
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/routes/routes.ts
+++ bennuTaskApp/src/routes/routes.ts
@@ -1,4 +1,5 @@
 export const ROUTES = {
   HOME: 'Home',
+  NEW_TASK: 'NewTask',
   TODO_DATAIL: 'TodoDetail',
 } as const;
--- bennuTaskApp/src/types/routes.ts
+++ bennuTaskApp/src/types/routes.ts
@@ -2,9 +2,11 @@ import {ROUTES} from '../routes/routes';
 
 export enum StackRoutesEnum {
   home = 'Home',
+  newTask = 'NewTask',
 }
 
 export type RootStackParamList = {
   [ROUTES.HOME]: undefined;
+  [ROUTES.NEW_TASK]: undefined;
   [ROUTES.TODO_DATAIL]: {id?: string};
 };
