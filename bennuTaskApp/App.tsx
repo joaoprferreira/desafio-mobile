5c08174fbfbc2b8aab9b3364483f796bf6df0b35
João Pedro <joaof082@gmail.com>
2025-05-12 12:36:02 -0300

    feat: adding component of Toast in application
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/App.tsx
+++ bennuTaskApp/App.tsx
@@ -9,12 +9,13 @@ import React from 'react';
 
 import {ThemeProvider} from 'styled-components';
 import {NavigationContainer} from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
 
 import {theme} from './src/styles/theme';
 import {Routes} from './src/routes';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
-import {store} from './src/redux/store';
+import {store} from './src/redux/store/store';
 
 function App(): React.JSX.Element {
   return (
@@ -23,6 +24,7 @@ function App(): React.JSX.Element {
         <ThemeProvider theme={theme}>
           <NavigationContainer>
             <Routes />
+            <Toast />
           </NavigationContainer>
         </ThemeProvider>
       </Provider>
