6346f04d9bdf4431222d58986f189af9c8bb0d2b
João Pedro <joaof082@gmail.com>
2025-05-13 02:23:00 -0300

    feat: adding inital state in component and adding validations to get informations of backend
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/Components/TaskItem/index.tsx
+++ bennuTaskApp/src/Components/TaskItem/index.tsx
@@ -1,49 +1,77 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 // import {Check, Trash2, Edit} from 'react-native-feather';
 import {useTheme} from 'styled-components';
 import * as Styled from './styles';
 import {Task} from '../../types/Task';
 import Icons from '../../assets/icons';
 import {Trash, Edit, Check} from '../../assets/icons';
-// import {theme} from '../../styles/theme';
+import {useTask} from '../../hooks/useTask';
+import {ActivityIndicator} from 'react-native';
+import {useRoute} from '@react-navigation/native';
+import {useAppSelector} from '../../redux/store/store';
 
-export const TaskItem: React.FC<Task> = ({
-  id,
-  title,
-  description,
-  completed,
-  onToggle,
-  onEdit,
+interface TaskItemProps {
+  item?: Task;
+  onDelete?: () => void;
+  onEdit?: () => void;
+  onToggle?: () => void;
+}
+
+export const TaskItem: React.FC<TaskItemProps> = ({
+  item = {
+    id: '',
+    title: '',
+    description: '',
+    completed: false,
+    createdAt: '',
+  },
   onDelete,
+  onEdit,
+  onToggle,
 }) => {
   const theme = useTheme();
+  const {loading} = useTask();
+  if (!item) {
+    return <ActivityIndicator animating={loading} />;
+  }
 
   return (
     <Styled.Container>
-      <Styled.CheckButton completed={completed} onPress={() => {}}>
-        {!completed && (
-          <Check width={30} height={30} color={theme.colors.primary} />
-        )}
-      </Styled.CheckButton>
+      {loading ? (
+        <ActivityIndicator
+          color={theme.colors.primaryDark}
+          animating={loading}
+        />
+      ) : (
+        <>
+          <Styled.CheckButton completed={item?.completed} onPress={() => {}}>
+            {item?.completed && (
+              <Check width={30} height={30} color={theme.colors.primary} />
+            )}
+          </Styled.CheckButton>
 
-      <Styled.Content>
-        <Styled.Title completed={completed}>{title}</Styled.Title>
-        {description && (
-          <Styled.Description completed={completed}>
-            {description}
-          </Styled.Description>
-        )}
-      </Styled.Content>
+          <Styled.Content>
+            <Styled.Title completed={item.completed}>
+              {String(item?.title) || ''}
+            </Styled.Title>
+            {item?.description && (
+              <Styled.Description completed={item?.completed}>
+                {item?.description}
+              </Styled.Description>
+            )}
+          </Styled.Content>
 
-      <Styled.ActionContainer>
-        <Styled.ActionButton onPress={() => {}}>
-          <Edit width={20} height={20} color={theme.colors.primary} />
-        </Styled.ActionButton>
+          <Styled.ActionContainer>
+            <Styled.ActionButton onPress={() => onEdit?.(item)}>
+              <Edit width={20} height={20} color={theme.colors.primary} />
+            </Styled.ActionButton>
 
-        <Styled.ActionButton onPress={() => {}}>
-          <Trash width={20} height={20} color={theme.colors.error} />
-        </Styled.ActionButton>
-      </Styled.ActionContainer>
+            <Styled.ActionButton onPress={() => {}}>
+              <Trash width={20} height={20} color={theme.colors.error} />
+            </Styled.ActionButton>
+          </Styled.ActionContainer>
+        </>
+      )}
     </Styled.Container>
   );
 };
