e6b82c7c7c62ec464efbb0ea927063e4a28a6004
João Pedro <joaof082@gmail.com>
2025-05-13 02:19:23 -0300

    feat: adding safeAreaPreview in home
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/App.tsx
+++ bennuTaskApp/App.tsx
@@ -13,23 +13,37 @@ import Toast from 'react-native-toast-message';
 
 import {theme} from './src/styles/theme';
 import {Routes} from './src/routes';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store/store';
+import {StyleSheet} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <NavigationContainer>
-            <Routes />
-            <Toast />
-          </NavigationContainer>
-        </ThemeProvider>
-      </Provider>
+      <SafeAreaView style={[styles.container, styles.horizontal]}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <NavigationContainer>
+              <Routes />
+              <Toast />
+            </NavigationContainer>
+          </ThemeProvider>
+        </Provider>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
 export default App;
