8fc365076fa73afca87ae0715294648184d802c6
João Pedro <joaof082@gmail.com>
2025-05-13 02:27:44 -0300

    refactor: adding service od android in enpoint of device
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/services/api.ts
+++ bennuTaskApp/src/services/api.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
+import {Platform} from 'react-native';
+
+const baseURL =
+  Platform.OS === 'android' ? 'http://10.0.2.2:9001' : 'http://localhost:9001';
 
 const api = axios.create({
-  baseURL: 'http://localhost:9001/',
+  baseURL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
