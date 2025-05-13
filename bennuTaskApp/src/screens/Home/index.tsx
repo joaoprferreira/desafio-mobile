18557751425f2918d853d05970a0d51e772d30e0
João Pedro <joaof082@gmail.com>
2025-05-13 02:25:39 -0300

    feat: adding call of allTasks in home and adding navigate to screen of edit
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/screens/Home/index.tsx
+++ bennuTaskApp/src/screens/Home/index.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {useFocusEffect} from '@react-navigation/native';
+import {FlatList, Text} from 'react-native';
 import {TaskItem} from '../../Components/TaskItem';
 import {Plus} from '../../assets/icons';
 import * as Styled from './styles';
 import {useNavigation} from '@react-navigation/native';
+import {useAppDispatch, useAppSelector} from '../../redux/store/store';
+import {fetchTasks} from '../../redux/slices/tasksSlice';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../types/routes';
 
 export const Home = () => {
-  const navigation = useNavigation();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList, 'Home'>>();
+  const dispatch = useAppDispatch();
+  const {items, error, loading} = useAppSelector(state => state.tasks);
+
+  console.log('Items::', items);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      dispatch(fetchTasks());
+      console.log('oi eu sou a home');
+      return () => {};
+    }, [dispatch]),
+  );
+
+  const navigateToEdit = (item: undefined) => {
+    navigation.navigate('EditTask', item);
+  };
+
   return (
     <Styled.PageContainer>
+      <FlatList
+        data={items.tasks || []}
+        renderItem={({item}) => (
+          <TaskItem item={item} onEdit={item => navigateToEdit(item)} />
+        )}
+        refreshing={loading}
+        keyExtractor={task => String(task.id)}
+      />
       <Text style={{color: 'black'}}>oi eu sou a home</Text>
 
       <TaskItem
