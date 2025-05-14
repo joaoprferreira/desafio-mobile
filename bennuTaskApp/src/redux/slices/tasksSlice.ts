b3c9cb3a1f0915ccacbf142f99463835d25c8d2d
João Pedro <joaof082@gmail.com>
2025-05-13 21:31:46 -0300

    refactor: removing addcases of slice to use RTK query with mutation
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/redux/slices/tasksSlice.ts
+++ bennuTaskApp/src/redux/slices/tasksSlice.ts
@@ -1,65 +1,44 @@
-import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import * as TaskService from '../../services/TaskService';
 import {IListTask, Task, Task as TaskType} from '../../types/Task';
 
-const initialState: IListTask = {
-  items: [],
+interface TasksUIState {
+  loading: boolean;
+  filter: string;
+}
+
+const initialState: TasksUIState = {
   loading: false,
-  error: null,
+  filter: '',
 };
 
-export const fetchTasks = createAsyncThunk<TaskType[]>(
-  'task/fetchTasks',
-  TaskService.fetchTasks,
-);
-export const addTask = createAsyncThunk<Task, Omit<Task, 'id' | 'createdAt'>>(
-  'task/create',
-  TaskService.createTask,
-);
-export const editTask = createAsyncThunk<
-  TaskType,
-  {id: string; task: Partial<Task>}
->('task/edit', async ({id, task}) => {
-  return TaskService.updateTask(id, task);
-});
+// export const fetchTasks = createAsyncThunk<TaskType[]>(
+//   'task/fetchTasks',
+//   TaskService.fetchTasks,
+// );
+// export const addTask = createAsyncThunk<Task, Omit<Task, 'id' | 'createdAt'>>(
+//   'task/create',
+//   TaskService.createTask,
+// );
+// export const editTask = createAsyncThunk<
+//   TaskType,
+//   {id: string; task: Partial<Task>}
+// >('task/edit', async ({id, task}) => {
+//   return TaskService.updateTask(id, task);
+// });
 
 const taskSlice = createSlice({
-  name: 'task',
+  name: 'taskUI',
   initialState,
   reducers: {
-    setLoadig: (state, action: PayloadAction<boolean>) => {
+    setLoading: (state, action: PayloadAction<boolean>) => {
       state.loading = action.payload;
     },
-  },
-  extraReducers: builder => {
-    builder
-      .addCase(fetchTasks.pending, state => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchTasks.fulfilled, (state, action) => {
-        state.loading = false;
-        state.items = action.payload;
-      })
-      .addCase(fetchTasks.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || 'Failed to fetch tasks';
-      })
-
-      .addCase(addTask.fulfilled, (state, action) => {
-        if (!Array.isArray(state.items)) {
-          state.items = [];
-        }
-        state.items.push(action.payload);
-      })
-
-      .addCase(editTask.fulfilled, (state, action) => {
-        const updatedTask = action.payload;
-        state.items = state.items.map(task =>
-          task.id === updatedTask.id ? updatedTask : task,
-        );
-      });
+    setFilter: (state, action: PayloadAction<string>) => {
+      state.filter = action.payload;
+    },
   },
 });
 
+export const {setLoading, setFilter} = taskSlice.actions;
 export default taskSlice.reducer;
