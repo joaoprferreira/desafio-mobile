f7fc0fa4640cd49e56256476981695ddfa1a3277
João Pedro <joaof082@gmail.com>
2025-05-13 21:33:48 -0300

    refacot: adding conditions of show components in home and removing codes that not used
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/screens/Home/index.tsx
+++ bennuTaskApp/src/screens/Home/index.tsx
@@ -7,47 +7,44 @@ import * as Styled from './styles';
 import {useNavigation} from '@react-navigation/native';
 import {useAppDispatch, useAppSelector} from '../../redux/store/store';
 import {fetchTasks} from '../../redux/slices/tasksSlice';
+import {Task as TaskType} from '../../types/Task';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../types/routes';
+import {useGetTasksQuery} from '../../services/api';
 
 export const Home = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList, 'Home'>>();
-  const dispatch = useAppDispatch();
-  const {items, error, loading} = useAppSelector(state => state.tasks);
+  // const dispatch = useAppDispatch();
+  const {data: items = [], error, isLoading, refetch} = useGetTasksQuery();
 
   console.log('Items::', items);
 
   useFocusEffect(
     React.useCallback(() => {
-      dispatch(fetchTasks());
-      console.log('oi eu sou a home');
-      return () => {};
-    }, [dispatch]),
+      refetch();
+    }, [refetch]),
   );
 
-  const navigateToEdit = (item: undefined) => {
+  const navigateToEdit = (item: TaskType) => {
     navigation.navigate('EditTask', item);
   };
 
   return (
     <Styled.PageContainer>
-      <FlatList
-        data={items.tasks || []}
-        renderItem={({item}) => (
-          <TaskItem item={item} onEdit={item => navigateToEdit(item)} />
-        )}
-        refreshing={loading}
-        keyExtractor={task => String(task.id)}
-      />
-      <Text style={{color: 'black'}}>oi eu sou a home</Text>
+      {items?.tasks?.length > 0 ? (
+        <FlatList
+          data={items.tasks}
+          renderItem={({item}) => (
+            <TaskItem item={item} onEdit={item => navigateToEdit(item)} />
+          )}
+          refreshing={isLoading}
+          keyExtractor={task => String(task.id)}
+        />
+      ) : (
+        <Text style={{color: '#000'}}> No tasks found</Text>
+      )}
 
-      <TaskItem
-        id="2"
-        title={'lavar louças'}
-        description={'com sabaão em pó'}
-        completed={false}
-      />
       <Styled.StyledButton
         onPress={() => navigation.navigate('NewTask')}
         testID="add-button">
