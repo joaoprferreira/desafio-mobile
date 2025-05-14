3fa404a0b79534538f4b29e70b9d336cf843ff43
João Pedro <joaof082@gmail.com>
2025-05-14 20:16:49 -0300

    feat: building hook to use in animations to page
    Signed-off-by: João Pedro <joaof082@gmail.com>

---
+++ bennuTaskApp/src/hooks/useAnimation.ts
@@ -0,0 +1,29 @@
+import {useRef, useEffect} from 'react';
+import {Animated, Easing} from 'react-native';
+
+export const useAnimation = () => {
+  const pulseAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(pulseAnim, {
+          toValue: 1.15,
+          duration: 700,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+        Animated.timing(pulseAnim, {
+          toValue: 1,
+          duration: 700,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+  }, [pulseAnim]);
+
+  return {
+    pulseAnim,
+  };
+};
