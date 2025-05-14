8bbae62ff1f43880c7cd9889bb95516bc4372812
João Pedro <joaof082@gmail.com>
2025-05-13 21:35:21 -0300

    feat: building query with mutation of redux toolkit
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/services/api.ts
+++ bennuTaskApp/src/services/api.ts
@@ -1,15 +1,43 @@
-import axios from 'axios';
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
 import {Platform} from 'react-native';
+import {Task} from '../types/Task';
 
 const baseURL =
   Platform.OS === 'android' ? 'http://10.0.2.2:9001' : 'http://localhost:9001';
 
-const api = axios.create({
-  baseURL,
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+export const tasksApi = createApi({
+  reducerPath: 'tasksApi',
+  baseQuery: fetchBaseQuery({baseUrl: baseURL}),
+  endpoints: builder => ({
+    getTasks: builder.query<Task[], void>({query: () => '/tasks'}),
+    addTask: builder.mutation<Task, Omit<Task, 'id' | 'createdAt'>>({
+      query: task => ({
+        url: '/task/create',
+        method: 'POST',
+        body: task,
+      }),
+    }),
+    updateTask: builder.mutation<Task, {id: string; task: Partial<Task>}>({
+      query: ({id, task}) => ({
+        url: `/task/update`,
+        method: 'PUT',
+        body: {id, ...task},
+      }),
+    }),
+    deleteTask: builder.mutation<Task, {id: string}>({
+      query: ({id}) => ({
+        url: `/task/delete`,
+        method: 'DELETE',
+        body: {id},
+      }),
+    }),
+  }),
 });
 
-export default api;
+export const {
+  useGetTasksQuery,
+  useAddTaskMutation,
+  useUpdateTaskMutation,
+  useDeleteTaskMutation,
+} = tasksApi;
