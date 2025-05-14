cdee6b18dfbad63cc05cae4bfb2983fdafbbd648
João Pedro <joaof082@gmail.com>
2025-05-14 20:09:52 -0300

    refactor: removing safeareaView of app tha not used
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/App.tsx
+++ bennuTaskApp/App.tsx
@@ -13,23 +13,21 @@ import Toast from 'react-native-toast-message';
 
 import {theme} from './src/styles/theme';
 import {Routes} from './src/routes';
-import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store/store';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-        <Provider store={store}>
-          <ThemeProvider theme={theme}>
-            <NavigationContainer>
-              <Routes />
-              <Toast />
-            </NavigationContainer>
-          </ThemeProvider>
-        </Provider>
-      </SafeAreaView>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <NavigationContainer>
+            <Routes />
+            <Toast />
+          </NavigationContainer>
+        </ThemeProvider>
+      </Provider>
     </SafeAreaProvider>
   );
 }
