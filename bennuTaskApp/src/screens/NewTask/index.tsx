8fc645e987ede78b3d47f45e3050891a42bcd0be
João Pedro <joaof082@gmail.com>
2025-05-14 20:20:20 -0300

    feat: adding skeleton in NewTaks page and adding confetti
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/screens/NewTask/index.tsx
+++ bennuTaskApp/src/screens/NewTask/index.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
-import {Text} from 'react-native';
+import {Text, Animated} from 'react-native';
 import * as Styled from './styles';
-import {useAppSelector} from '../../redux/store/store';
 import {useRoute} from '@react-navigation/native';
 import useNewTask from '../../hooks/useNewTask';
 import {Task} from '../../types/Task';
 import {useGetTasksQuery} from '../../services/api';
+import {SkeletonPageContainer} from '../../Components/skeletonPageContainer';
+import {useAnimation} from '../../hooks/useAnimation';
 
 export const NewTask = () => {
   const route = useRoute();
@@ -13,7 +14,7 @@ export const NewTask = () => {
   const params = (route.params ?? {}) as Partial<Task>;
   const {id} = params;
   const {data: items = [], isLoading, error} = useGetTasksQuery();
-
+  const {pulseAnim} = useAnimation();
   const task = Array.isArray(items) ? items.find(item => item.id === id) : null;
 
   const {
@@ -22,6 +23,7 @@ export const NewTask = () => {
     currentDescription,
     setCurrentDescription,
     setCurrentTitle,
+    isDisableButton,
   } = useNewTask({params});
 
   useEffect(() => {
@@ -31,26 +33,45 @@ export const NewTask = () => {
     }
   }, [task, setCurrentDescription, setCurrentTitle]);
 
-  return (
-    <Styled.PageContainer>
-      <Text>OI Aqui você cria uma nova task</Text>
-      {error && <Text>{error}</Text>}
+  const isEditMode = Boolean(id);
 
-      <Styled.Input
-        testID="title"
-        placeholder="Título"
-        value={currentTitle}
-        onChangeText={setCurrentTitle}
-      />
-      <Styled.Input
-        testID="description"
-        placeholder="Descrição"
-        value={currentDescription}
-        onChangeText={setCurrentDescription}
-      />
-      <Styled.StyledButton onPress={handlerButtonConfirmation}>
-        <Text>{id ? 'Salvar alterações' : 'Criar nova task'}</Text>
-      </Styled.StyledButton>
-    </Styled.PageContainer>
+  return (
+    <SkeletonPageContainer
+      pageTitle={isEditMode ? 'Editar tarefa' : 'Nova tarefa'}
+      subtitle={
+        isEditMode
+          ? 'Por favor, preencha os campos abaixo para editar a tarefa selecionada. '
+          : 'Insira as informações da nova tarefa nos campos abaixo.'
+      }>
+      <Styled.PageContainer>
+        {error && <Text>{error}</Text>}
+
+        <Styled.Input
+          testID="title"
+          placeholder="Título"
+          value={currentTitle}
+          onChangeText={setCurrentTitle}
+        />
+        <Styled.Input
+          testID="description"
+          placeholder="Descrição"
+          value={currentDescription}
+          onChangeText={setCurrentDescription}
+        />
+
+        <Styled.StyledButton
+          onPress={handlerButtonConfirmation}
+          disabled={isDisableButton}>
+          <Animated.View
+            style={{
+              transform: [{scale: isDisableButton ? 1 : pulseAnim}],
+            }}>
+            <Styled.TitleButton>
+              {id ? 'Salvar alterações' : 'Criar nova task'}
+            </Styled.TitleButton>
+          </Animated.View>
+        </Styled.StyledButton>
+      </Styled.PageContainer>
+    </SkeletonPageContainer>
   );
 };
