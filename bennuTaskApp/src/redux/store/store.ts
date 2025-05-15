5685f519fa56eb6033d3930706e62455860a7b5a
João Pedro <joaof082@gmail.com>
2025-05-15 00:35:17 -0300

    feat: adding persistStore and async Storage
    Signed-off-by: João Pedro <joaof082@gmail.com>

--- bennuTaskApp/src/redux/store/store.ts
+++ bennuTaskApp/src/redux/store/store.ts
@@ -1,16 +1,32 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import tasksReducer from '../slices/tasksSlice';
 import {TypedUseSelectorHook, useSelector, useDispatch} from 'react-redux';
 import {tasksApi} from '../../services/api';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['tasks'],
+};
+
+const rootReducer = combineReducers({
+  [tasksApi.reducerPath]: tasksApi.reducer,
+  tasks: tasksReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    [tasksApi.reducerPath]: tasksApi.reducer,
-    tasks: tasksReducer,
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(tasksApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(tasksApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
